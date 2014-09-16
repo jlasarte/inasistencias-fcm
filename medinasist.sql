b2e53cf7bff03d3c38a1a5e85473d5c209590b68
Julia Lasarte <julia.lasarte@gmail.com>
2014-09-16 08:45:47 -0300

    script db

---
+++ medinasist.sql
@@ -0,0 +1,285 @@
+-- phpMyAdmin SQL Dump
+-- version 4.0.10deb1
+-- http://www.phpmyadmin.net
+--
+-- Servidor: localhost
+-- Tiempo de generación: 16-09-2014 a las 08:43:06
+-- Versión del servidor: 5.5.38-0ubuntu0.14.04.1
+-- Versión de PHP: 5.5.9-1ubuntu4.4
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8 */;
+
+--
+-- Base de datos: `medinasist`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `acl_classes`
+--
+
+CREATE TABLE IF NOT EXISTS `acl_classes` (
+  `id` int(10) unsigned NOT NULL AUTO_INCREMENT,
+  `class_type` varchar(200) COLLATE utf8_unicode_ci NOT NULL,
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `UNIQ_69DD750638A36066` (`class_type`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci AUTO_INCREMENT=1 ;
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `acl_entries`
+--
+
+CREATE TABLE IF NOT EXISTS `acl_entries` (
+  `id` int(10) unsigned NOT NULL AUTO_INCREMENT,
+  `class_id` int(10) unsigned NOT NULL,
+  `object_identity_id` int(10) unsigned DEFAULT NULL,
+  `security_identity_id` int(10) unsigned NOT NULL,
+  `field_name` varchar(50) COLLATE utf8_unicode_ci DEFAULT NULL,
+  `ace_order` smallint(5) unsigned NOT NULL,
+  `mask` int(11) NOT NULL,
+  `granting` tinyint(1) NOT NULL,
+  `granting_strategy` varchar(30) COLLATE utf8_unicode_ci NOT NULL,
+  `audit_success` tinyint(1) NOT NULL,
+  `audit_failure` tinyint(1) NOT NULL,
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `UNIQ_46C8B806EA000B103D9AB4A64DEF17BCE4289BF4` (`class_id`,`object_identity_id`,`field_name`,`ace_order`),
+  KEY `IDX_46C8B806EA000B103D9AB4A6DF9183C9` (`class_id`,`object_identity_id`,`security_identity_id`),
+  KEY `IDX_46C8B806EA000B10` (`class_id`),
+  KEY `IDX_46C8B8063D9AB4A6` (`object_identity_id`),
+  KEY `IDX_46C8B806DF9183C9` (`security_identity_id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci AUTO_INCREMENT=1 ;
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `acl_object_identities`
+--
+
+CREATE TABLE IF NOT EXISTS `acl_object_identities` (
+  `id` int(10) unsigned NOT NULL AUTO_INCREMENT,
+  `parent_object_identity_id` int(10) unsigned DEFAULT NULL,
+  `class_id` int(10) unsigned NOT NULL,
+  `object_identifier` varchar(100) COLLATE utf8_unicode_ci NOT NULL,
+  `entries_inheriting` tinyint(1) NOT NULL,
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `UNIQ_9407E5494B12AD6EA000B10` (`object_identifier`,`class_id`),
+  KEY `IDX_9407E54977FA751A` (`parent_object_identity_id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci AUTO_INCREMENT=1 ;
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `acl_object_identity_ancestors`
+--
+
+CREATE TABLE IF NOT EXISTS `acl_object_identity_ancestors` (
+  `object_identity_id` int(10) unsigned NOT NULL,
+  `ancestor_id` int(10) unsigned NOT NULL,
+  PRIMARY KEY (`object_identity_id`,`ancestor_id`),
+  KEY `IDX_825DE2993D9AB4A6` (`object_identity_id`),
+  KEY `IDX_825DE299C671CEA1` (`ancestor_id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `acl_security_identities`
+--
+
+CREATE TABLE IF NOT EXISTS `acl_security_identities` (
+  `id` int(10) unsigned NOT NULL AUTO_INCREMENT,
+  `identifier` varchar(200) COLLATE utf8_unicode_ci NOT NULL,
+  `username` tinyint(1) NOT NULL,
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `UNIQ_8835EE78772E836AF85E0677` (`identifier`,`username`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci AUTO_INCREMENT=1 ;
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `employee`
+--
+
+CREATE TABLE IF NOT EXISTS `employee` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `name` varchar(255) COLLATE utf8_unicode_ci NOT NULL,
+  `last_name` varchar(255) COLLATE utf8_unicode_ci NOT NULL,
+  `positiom` varchar(255) COLLATE utf8_unicode_ci NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB  DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci AUTO_INCREMENT=2 ;
+
+--
+-- Volcado de datos para la tabla `employee`
+--
+
+INSERT INTO `employee` (`id`, `name`, `last_name`, `positiom`) VALUES
+(1, 'Carlos', 'Lopez', 'Jefe');
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `fos_user`
+--
+
+CREATE TABLE IF NOT EXISTS `fos_user` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `username` varchar(255) COLLATE utf8_unicode_ci NOT NULL,
+  `username_canonical` varchar(255) COLLATE utf8_unicode_ci NOT NULL,
+  `email` varchar(255) COLLATE utf8_unicode_ci NOT NULL,
+  `email_canonical` varchar(255) COLLATE utf8_unicode_ci NOT NULL,
+  `enabled` tinyint(1) NOT NULL,
+  `salt` varchar(255) COLLATE utf8_unicode_ci NOT NULL,
+  `password` varchar(255) COLLATE utf8_unicode_ci NOT NULL,
+  `last_login` datetime DEFAULT NULL,
+  `locked` tinyint(1) NOT NULL,
+  `expired` tinyint(1) NOT NULL,
+  `expires_at` datetime DEFAULT NULL,
+  `confirmation_token` varchar(255) COLLATE utf8_unicode_ci DEFAULT NULL,
+  `password_requested_at` datetime DEFAULT NULL,
+  `roles` longtext COLLATE utf8_unicode_ci NOT NULL COMMENT '(DC2Type:array)',
+  `credentials_expired` tinyint(1) NOT NULL,
+  `credentials_expire_at` datetime DEFAULT NULL,
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `UNIQ_957A647992FC23A8` (`username_canonical`),
+  UNIQUE KEY `UNIQ_957A6479A0D96FBF` (`email_canonical`)
+) ENGINE=InnoDB  DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci AUTO_INCREMENT=2 ;
+
+--
+-- Volcado de datos para la tabla `fos_user`
+--
+
+INSERT INTO `fos_user` (`id`, `username`, `username_canonical`, `email`, `email_canonical`, `enabled`, `salt`, `password`, `last_login`, `locked`, `expired`, `expires_at`, `confirmation_token`, `password_requested_at`, `roles`, `credentials_expired`, `credentials_expire_at`) VALUES
+(1, 'julia.lasarte', 'julia.lasarte', 'julia.lasarte@gmail.com', 'julia.lasarte@gmail.com', 1, '2gnbegtx14pwg4go4c4o4owc0sgc44k', '22aNaMCTotK5ptJPoeOF96Wt34cv7Ds/HB8HkGZQTa89Q+7/t8ePHNnB7KAYLX0yiIjmqGDRsEBwuo/SdfA8qg==', '2014-09-15 19:06:12', 0, 0, NULL, NULL, NULL, 'a:0:{}', 0, NULL);
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `fos_user_group`
+--
+
+CREATE TABLE IF NOT EXISTS `fos_user_group` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `name` varchar(255) COLLATE utf8_unicode_ci NOT NULL,
+  `roles` longtext COLLATE utf8_unicode_ci NOT NULL COMMENT '(DC2Type:array)',
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `UNIQ_583D1F3E5E237E06` (`name`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci AUTO_INCREMENT=1 ;
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `fos_user_user`
+--
+
+CREATE TABLE IF NOT EXISTS `fos_user_user` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `username` varchar(255) COLLATE utf8_unicode_ci NOT NULL,
+  `username_canonical` varchar(255) COLLATE utf8_unicode_ci NOT NULL,
+  `email` varchar(255) COLLATE utf8_unicode_ci NOT NULL,
+  `email_canonical` varchar(255) COLLATE utf8_unicode_ci NOT NULL,
+  `enabled` tinyint(1) NOT NULL,
+  `salt` varchar(255) COLLATE utf8_unicode_ci NOT NULL,
+  `password` varchar(255) COLLATE utf8_unicode_ci NOT NULL,
+  `last_login` datetime DEFAULT NULL,
+  `locked` tinyint(1) NOT NULL,
+  `expired` tinyint(1) NOT NULL,
+  `expires_at` datetime DEFAULT NULL,
+  `confirmation_token` varchar(255) COLLATE utf8_unicode_ci DEFAULT NULL,
+  `password_requested_at` datetime DEFAULT NULL,
+  `roles` longtext COLLATE utf8_unicode_ci NOT NULL COMMENT '(DC2Type:array)',
+  `credentials_expired` tinyint(1) NOT NULL,
+  `credentials_expire_at` datetime DEFAULT NULL,
+  `created_at` datetime NOT NULL,
+  `updated_at` datetime NOT NULL,
+  `date_of_birth` datetime DEFAULT NULL,
+  `firstname` varchar(64) COLLATE utf8_unicode_ci DEFAULT NULL,
+  `lastname` varchar(64) COLLATE utf8_unicode_ci DEFAULT NULL,
+  `website` varchar(64) COLLATE utf8_unicode_ci DEFAULT NULL,
+  `biography` varchar(255) COLLATE utf8_unicode_ci DEFAULT NULL,
+  `gender` varchar(1) COLLATE utf8_unicode_ci DEFAULT NULL,
+  `locale` varchar(8) COLLATE utf8_unicode_ci DEFAULT NULL,
+  `timezone` varchar(64) COLLATE utf8_unicode_ci DEFAULT NULL,
+  `phone` varchar(64) COLLATE utf8_unicode_ci DEFAULT NULL,
+  `facebook_uid` varchar(255) COLLATE utf8_unicode_ci DEFAULT NULL,
+  `facebook_name` varchar(255) COLLATE utf8_unicode_ci DEFAULT NULL,
+  `facebook_data` longtext COLLATE utf8_unicode_ci COMMENT '(DC2Type:json)',
+  `twitter_uid` varchar(255) COLLATE utf8_unicode_ci DEFAULT NULL,
+  `twitter_name` varchar(255) COLLATE utf8_unicode_ci DEFAULT NULL,
+  `twitter_data` longtext COLLATE utf8_unicode_ci COMMENT '(DC2Type:json)',
+  `gplus_uid` varchar(255) COLLATE utf8_unicode_ci DEFAULT NULL,
+  `gplus_name` varchar(255) COLLATE utf8_unicode_ci DEFAULT NULL,
+  `gplus_data` longtext COLLATE utf8_unicode_ci COMMENT '(DC2Type:json)',
+  `token` varchar(255) COLLATE utf8_unicode_ci DEFAULT NULL,
+  `two_step_code` varchar(255) COLLATE utf8_unicode_ci DEFAULT NULL,
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `UNIQ_C560D76192FC23A8` (`username_canonical`),
+  UNIQUE KEY `UNIQ_C560D761A0D96FBF` (`email_canonical`)
+) ENGINE=InnoDB  DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci AUTO_INCREMENT=3 ;
+
+--
+-- Volcado de datos para la tabla `fos_user_user`
+--
+
+INSERT INTO `fos_user_user` (`id`, `username`, `username_canonical`, `email`, `email_canonical`, `enabled`, `salt`, `password`, `last_login`, `locked`, `expired`, `expires_at`, `confirmation_token`, `password_requested_at`, `roles`, `credentials_expired`, `credentials_expire_at`, `created_at`, `updated_at`, `date_of_birth`, `firstname`, `lastname`, `website`, `biography`, `gender`, `locale`, `timezone`, `phone`, `facebook_uid`, `facebook_name`, `facebook_data`, `twitter_uid`, `twitter_name`, `twitter_data`, `gplus_uid`, `gplus_name`, `gplus_data`, `token`, `two_step_code`) VALUES
+(1, 'julia', 'julia', 'julia.lasarte@gmail.com', 'julia.lasarte@gmail.com', 1, 'n9ggrsf46v4w4k48kw4w08k0gkk0ksg', '/2rviWx7z2+SUTxjMSqlNSc9mPPEodUVI9ISEmIIJrgX4kh3dyMw0rTmWkQ7m4IW8Hjw4BS6SaDYn8XIYPtTdg==', '2014-09-16 08:32:18', 0, 0, NULL, NULL, NULL, 'a:1:{i:0;s:16:"ROLE_SUPER_ADMIN";}', 0, NULL, '2014-09-16 08:16:45', '2014-09-16 08:32:18', NULL, NULL, NULL, NULL, NULL, 'u', NULL, NULL, NULL, NULL, NULL, 'null', NULL, NULL, 'null', NULL, NULL, 'null', NULL, NULL),
+(2, 'admin', 'admin', 'admin@admin.com', 'admin@admin.com', 1, '9vmzrcdksf4ksskoc84o4o4goosg4ck', 'DAdTJURtfkB0CDxh+r1flFkcNfF7PLTeXhpb61pYRA/YbXvXoHKRG9ciH7LTmW7zSWMD/nt/TlIKrQkmoS4iIw==', NULL, 0, 0, NULL, NULL, NULL, 'a:1:{i:0;s:16:"ROLE_SUPER_ADMIN";}', 0, NULL, '2014-09-16 08:38:35', '2014-09-16 08:38:35', NULL, NULL, NULL, NULL, NULL, 'u', NULL, NULL, NULL, NULL, NULL, 'null', NULL, NULL, 'null', NULL, NULL, 'null', NULL, NULL);
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `fos_user_user_group`
+--
+
+CREATE TABLE IF NOT EXISTS `fos_user_user_group` (
+  `user_id` int(11) NOT NULL,
+  `group_id` int(11) NOT NULL,
+  PRIMARY KEY (`user_id`,`group_id`),
+  KEY `IDX_B3C77447A76ED395` (`user_id`),
+  KEY `IDX_B3C77447FE54D947` (`group_id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
+
+--
+-- Restricciones para tablas volcadas
+--
+
+--
+-- Filtros para la tabla `acl_entries`
+--
+ALTER TABLE `acl_entries`
+  ADD CONSTRAINT `FK_46C8B806DF9183C9` FOREIGN KEY (`security_identity_id`) REFERENCES `acl_security_identities` (`id`) ON DELETE CASCADE ON UPDATE CASCADE,
+  ADD CONSTRAINT `FK_46C8B8063D9AB4A6` FOREIGN KEY (`object_identity_id`) REFERENCES `acl_object_identities` (`id`) ON DELETE CASCADE ON UPDATE CASCADE,
+  ADD CONSTRAINT `FK_46C8B806EA000B10` FOREIGN KEY (`class_id`) REFERENCES `acl_classes` (`id`) ON DELETE CASCADE ON UPDATE CASCADE;
+
+--
+-- Filtros para la tabla `acl_object_identities`
+--
+ALTER TABLE `acl_object_identities`
+  ADD CONSTRAINT `FK_9407E54977FA751A` FOREIGN KEY (`parent_object_identity_id`) REFERENCES `acl_object_identities` (`id`);
+
+--
+-- Filtros para la tabla `acl_object_identity_ancestors`
+--
+ALTER TABLE `acl_object_identity_ancestors`
+  ADD CONSTRAINT `FK_825DE299C671CEA1` FOREIGN KEY (`ancestor_id`) REFERENCES `acl_object_identities` (`id`) ON DELETE CASCADE ON UPDATE CASCADE,
+  ADD CONSTRAINT `FK_825DE2993D9AB4A6` FOREIGN KEY (`object_identity_id`) REFERENCES `acl_object_identities` (`id`) ON DELETE CASCADE ON UPDATE CASCADE;
+
+--
+-- Filtros para la tabla `fos_user_user_group`
+--
+ALTER TABLE `fos_user_user_group`
+  ADD CONSTRAINT `FK_B3C77447FE54D947` FOREIGN KEY (`group_id`) REFERENCES `fos_user_group` (`id`) ON DELETE CASCADE,
+  ADD CONSTRAINT `FK_B3C77447A76ED395` FOREIGN KEY (`user_id`) REFERENCES `fos_user_user` (`id`) ON DELETE CASCADE;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
