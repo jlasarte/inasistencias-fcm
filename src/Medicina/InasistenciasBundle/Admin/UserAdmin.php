34fed66af466442d4256b9cadbfe2028c7b40319
Julia Lasarte <julia.lasate@gmail.com>
2014-10-07 10:40:32 -0300

    Modificación de la clase usuario

---
+++ src/Medicina/InasistenciasBundle/Admin/UserAdmin.php
@@ -0,0 +1,197 @@
+<?php
+
+/*
+ * This file is part of the Sonata package.
+ *
+ * (c) Thomas Rabaix <thomas.rabaix@sonata-project.org>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace Medicina\InasistenciasBundle\Admin;
+
+use Sonata\AdminBundle\Admin\Admin;
+use Sonata\AdminBundle\Form\FormMapper;
+use Sonata\AdminBundle\Datagrid\DatagridMapper;
+use Sonata\AdminBundle\Datagrid\ListMapper;
+use Sonata\AdminBundle\Show\ShowMapper;
+
+use FOS\UserBundle\Model\UserManagerInterface;
+
+class UserAdmin extends Admin
+{
+    protected $userManager;
+
+    /**
+     * {@inheritdoc}
+     */
+    public function getFormBuilder()
+    {
+        $this->formOptions['data_class'] = $this->getClass();
+
+        $options = $this->formOptions;
+        $options['validation_groups'] = (!$this->getSubject() || is_null($this->getSubject()->getId())) ? 'Registration' : 'Profile';
+
+        $formBuilder = $this->getFormContractor()->getFormBuilder( $this->getUniqid(), $options);
+
+        $this->defineFormBuilder($formBuilder);
+
+        return $formBuilder;
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function getExportFields()
+    {
+        // avoid security field to be exported
+        return array_filter(parent::getExportFields(), function($v) {
+            return !in_array($v, array('password', 'salt'));
+        });
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    protected function configureListFields(ListMapper $listMapper)
+    {
+        $listMapper
+            ->addIdentifier('username', null, array('label'=>'Nombre de Usuario'))
+            ->add('email', null, array('label'=>'email'))
+            ->add('groups', null, array('label'=>'grupos'))
+            ->add('enabled', null, array('editable' => true, 'label'=>'Habilitado'))
+            ->add('locked', null, array('editable' => true, 'label'=>'Bloqueado'))
+        ;
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    protected function configureDatagridFilters(DatagridMapper $filterMapper)
+    {
+        $filterMapper
+            ->add('username', null, array('label'=>'Nombre de Usuario'))
+            ->add('email', null, array('label'=>'email'))
+            ->add('groups', null, array('label'=>'Grupos'))
+        ;
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    protected function configureShowFields(ShowMapper $showMapper)
+    {
+        $showMapper
+            ->with('General')
+                ->add('username')
+                ->add('email')
+            ->end()
+            ->with('Groups')
+                ->add('groups')
+            ->end()
+            ->with('Profile')
+                ->add('dateOfBirth')
+                ->add('firstname')
+                ->add('lastname')
+                ->add('website')
+                ->add('biography')
+                ->add('gender')
+                ->add('locale')
+                ->add('timezone')
+                ->add('phone')
+            ->end()
+            ->with('Social')
+                ->add('facebookUid')
+                ->add('facebookName')
+                ->add('twitterUid')
+                ->add('twitterName')
+                ->add('gplusUid')
+                ->add('gplusName')
+            ->end()
+            ->with('Security')
+                ->add('token')
+                ->add('twoStepVerificationCode')
+            ->end()
+        ;
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    protected function configureFormFields(FormMapper $formMapper)
+    {
+        // define group zoning
+        $formMapper
+            ->tab('Usuario')
+                ->with('Perfil', array('class' => 'col-md-12'))->end()
+            ->end()
+            ->tab('Seguridad')
+                ->with('Estado', array('class' => 'col-md-4'))->end()
+                ->with('Groups', array('class' => 'col-md-4'))->end()
+                ->with('Keys', array('class' => 'col-md-4'))->end()
+                ->with('Roles', array('class' => 'col-md-12'))->end()
+            ->end()
+        ;
+
+        $now = new \DateTime();
+
+        $formMapper
+            ->tab('Usuario')
+                ->with('Perfil')
+                    ->add('firstname', null, array('required' => false, 'label'=>'Nombre'))
+                    ->add('lastname', null, array('required' => false, 'label'=>'Apellido'))
+                    ->add('username', 'text', array('label'=>'Nombre de Usuario'))
+                    ->add('email', 'text', array('label'=>'Email'))
+                    ->add('plainPassword', 'text', array(
+                        'required' => (!$this->getSubject() || is_null($this->getSubject()->getId())), 'label'=> 'contraseña'
+                    ))
+                ->end()
+            ->end()
+        ;
+
+        if ($this->getSubject() && !$this->getSubject()->hasRole('ROLE_SUPER_ADMIN')) {
+            $formMapper
+                ->tab('Seguridad')
+                    ->with('Estado')
+                        ->add('locked', null, array('required' => false, 'label'=>'Bloqueado'))
+                        ->add('enabled', null, array('required' => false, 'label'=>'Habilitado'))
+                        ->add('credentialsExpired', null, array('required' => false, 'label'=>'credenciales vencidas'))
+                    ->end()
+                    ->with('Groups')
+                        ->add('groups', 'sonata_type_model', array(
+                            'required' => false,
+                            'expanded' => true,
+                            'multiple' => true
+                        ))
+                    ->end()
+                ->end()
+            ;
+        }
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function preUpdate($user)
+    {
+        $this->getUserManager()->updateCanonicalFields($user);
+        $this->getUserManager()->updatePassword($user);
+    }
+
+    /**
+     * @param UserManagerInterface $userManager
+     */
+    public function setUserManager(UserManagerInterface $userManager)
+    {
+        $this->userManager = $userManager;
+    }
+
+    /**
+     * @return UserManagerInterface
+     */
+    public function getUserManager()
+    {
+        return $this->userManager;
+    }
+}
